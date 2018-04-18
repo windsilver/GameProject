ad5a1e6ff084fb78a82ba932b19e8ce0b9a2d34a
windsilver <33573367+windsilver@users.noreply.github.com>
2018-04-18 15:51:39 +0800

    0418_menu-Update
    更新程式碼

--- menu.lua
+++ menu.lua
@@ -4,25 +4,20 @@ local scene = composer.newScene()
 local centerX = display.contentCenterX 
 local centerY = display.contentCenterY
 
-local menu = display.newImage("Menu.png",centerX, centerY)  --背景圖,X,Y
+local menu = display.newImage("Menu.png") --背景圖
   menu.width = centerX*2
   menu.height = centerY*2
 
-local start = display.newImage("Start.png",centerX,centerY-centerY/5)
+local start = display.newImage("Start.png",centerX,centerY-centerY/5) --開始按鍵
   start.width = centerX/2
   start.height = centerY/3
 
-
 function start:touch(event)
-	if event.phase == "began" then--如果點擊               
-            composer.gotoScene("level_1",{effect = "slideLeft",time = 800})
+	if event.phase == "began" then --如果點擊                            
+            composer.gotoScene("level_1",{effect = "fade",time = 500})
+            start:removeEventListener("touch",start) --start刪除點擊
     end
 end
-
-
-
-
-
 
 
 
@@ -34,24 +29,21 @@ end
 -----------------------------------------
 function scene:create( event )
     local sceneGroup = self.view
-    start:addEventListener("touch", start )
-    print("menu_create")
+    menu.x = centerX --背景圖X
+    menu.y = centerY --背景圖Y
+    sceneGroup:insert(menu) --menu匯入場景
+    sceneGroup:insert(start) --start匯入場景
 end
 
 function scene:show( event )
     local sceneGroup = self.view
-    print("menu_show")
+    start:addEventListener("touch", start ) --start加入點擊
 end
 
 function scene:hide( event )
-    local sceneGroup = self.view
-    start:removeEventListener("touch",start)
-    print("menu_hide")
 end
 
 function scene:destroy( event )
-    local sceneGroup = self.view
-    print("menu_destroy")
 end
 
 scene:addEventListener( "create", scene )
